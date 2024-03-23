4e61187343811cbb75674842119b5e57df8c320f
okezieuc <53785400+okezieuc@users.noreply.github.com>
2024-03-23 10:21:29 -0500

    feat: adds support for setting a server as a replica

--- app/server.c
+++ app/server.c
@@ -12,8 +12,14 @@
 #include "resp/resp.h"
 #include "dict.h"
 
+struct ServerMetadata
+{
+	int is_replica;
+};
+
 struct ConnArgs
 {
+	struct ServerMetadata server_meta_data;
 	int client_fd;
 	struct Dict *store;
 };
@@ -33,6 +39,7 @@ void *handle_req(void *arg)
 	// store a copy of the client_fd in this context
 	int len, client_fd = (int)(((struct ConnArgs *)arg)->client_fd);
 	struct Dict *store = (struct Dict *)(((struct ConnArgs *)arg)->store);
+	struct ServerMetadata server_meta_data = ((struct ConnArgs *)arg)->server_meta_data;
 	char req_buffer[1024], res_buffer[1024], *ptr;
 
 	// receive messge from client
@@ -148,7 +155,17 @@ void *handle_req(void *arg)
 
 			if (strcmp(arg1->data, "replication") == 0)
 			{
-				struct RESPBulkStringNode *res_node = create_resp_bulk_string_node("role:master");
+				struct RESPBulkStringNode *res_node;
+
+				if (server_meta_data.is_replica == 0)
+				{
+					res_node = create_resp_bulk_string_node("role:master");
+				}
+				else
+				{
+					res_node = create_resp_bulk_string_node("role:slave");
+				}
+
 				char *res_body = encode_resp_bulk_string(res_node);
 
 				send(client_fd, res_body, strlen(res_body), 0);
@@ -173,6 +190,8 @@ int main(int argc, char *argv[])
 
 	int server_fd, client_fd, client_addr_len, port = 6379;
 	struct sockaddr_in client_addr;
+	struct ServerMetadata server_meta_data;
+	server_meta_data.is_replica = 0;
 
 	// handle command line arguments
 	for (int i = 0; i < argc; i++)
@@ -181,6 +200,11 @@ int main(int argc, char *argv[])
 		{
 			port = atoi(argv[i + 1]);
 			i++;
+		}
+		else if (strcmp(argv[i], "--replicaof") == 0)
+		{
+			server_meta_data.is_replica = 1;
+			i += 2;
 		}
 	}
 
@@ -194,9 +218,9 @@ int main(int argc, char *argv[])
 	// Since the tester restarts your program quite often, setting REUSE_PORT
 	// ensures that we don't run into 'Address already in use' errors
 	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0)
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
 	{
-		printf("SO_REUSEPORT failed: %s \n", strerror(errno));
+		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
 		return 1;
 	}
 
@@ -232,7 +256,7 @@ int main(int argc, char *argv[])
 	while ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len)) != -1)
 	{
 		printf("Client connected\n");
-		struct ConnArgs args = {client_fd, store};
+		struct ConnArgs args = {server_meta_data, client_fd, store};
 		pthread_create(&t_ids[current_thread++], NULL, handle_req, (void *)&args);
 	}
 
