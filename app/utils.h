b8b5114d0d9d66fae73b5ca6499f1f0e513749e7
okezieuc <53785400+okezieuc@users.noreply.github.com>
2024-03-29 10:00:46 -0500

    adds hex <-> binary converters

--- app/utils.h
+++ app/utils.h
@@ -1,7 +1,11 @@
 #ifndef CC_REDIS_UTILS
 #define CC_REDIS_UTILS
+#include <stdlib.h>
 
 int parse_number(char **digit_ptr, int adjust_pointer);
 long int get_current_time();
+char *bin2hex(const unsigned char *bin, size_t len);
+int hexchr2bin(const char hex, char *out);
+size_t hexs2bin(const char *hex, unsigned char **out);
 
 #endif
